c72289dc28e8c92d7db7e27de6f575a7208155fc
Joona Pääkkönen <joona.paakkonen@fake.fi>
2022-11-20 08:35:51 +0200

    Remove unused garbage

--- PixelEngine/nestest.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="nestest" tilewidth="8" tileheight="8" tilecount="256" columns="16">
- <image source="CHR000.bmp" trans="a675fe" width="128" height="128"/>
-</tileset>
