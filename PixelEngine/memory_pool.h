78d316100f4bce22b4eed0e1002e8d7b55ef7297
Joona Pääkkönen <jonah.paakkonen@gmail.com>
2024-06-01 00:09:54 +0300

    Add generational memory pools

--- PixelEngine/memory_pool.h
+++ PixelEngine/memory_pool.h
@@ -1,73 +1,127 @@
 #pragma once
 #include "typedef.h"
+#include "system.h"
 
 template <typename T>
-struct MemoryPool
-{
-private:
-    T* objs;
-    u32* handles;
-    u32 maxCount;
-    u32 count;
+class PoolHandle {
+	u64 raw;
 public:
-    void Init(u32 size) {
-        maxCount = size;
-        count = 0;
+	PoolHandle() = default;
+	PoolHandle(const PoolHandle& other) = default;
+	PoolHandle(u64 raw) : raw(raw) {}
+	PoolHandle(u32 index, u32 gen) {
+		raw = ((u64)gen << 32ull) | (u64)index;
+	}
 
-        objs = (T*)calloc(size, sizeof(T));
-        handles = (u32*)calloc(size, sizeof(u32));
+	inline u32 Index() const {
+		return (u32)(raw % 0x100000000);
+	}
 
-        for (u32 i = 0; i < size; i++)
-        {
-            handles[i] = i;
-        }
-    }
+	inline u32 Generation() const {
+		return (u32)(raw >> 32ull);
+	}
 
-    void Free() {
-        free(objs);
-        free(handles);
-    }
+	inline const u64 Raw() const {
+		return raw;
+	}
 
-    T* AllocObject()
-    {
-        if (count >= maxCount)
-            return nullptr;
+	bool operator==(const PoolHandle& other) const {
+		return raw == other.raw;
+	}
+	bool operator!=(const PoolHandle& other) const {
+		return raw != other.raw;
+	}
 
-        u32 handle = handles[count++];
-        return &objs[handle];
-    }
-
-    T& operator[](u32 index)
-    {
-        s32 handle = GetHandle(index);
-        if (handle == -1) {
-            DEBUG_ERROR("U dun fuk up\n");
-        }
-
-        return objs[handle];
-    }
-
-    s32 GetHandle(u32 i)
-    {
-        if (i >= count)
-            return -1;
-
-        return handles[i];
-    }
-
-    u32 Count() const
-    {
-        return count;
-    }
-
-    void FreeObject(u32 index) {
-        s32 handle = GetHandle(index);
-        if (handle == -1) {
-            return;
-        }
-
-        handles[index] = handles[--count];
-        handles[count] = handle;
-    }
+	inline static PoolHandle Null() {
+		return PoolHandle(0);
+	}
 };
 
+template<typename T, typename THandle = PoolHandle<T>>
+class Pool
+{
+private:
+	T* objs;
+	THandle* handles;
+	u32* erase;
+
+	u32 size;
+	u32 count;
+public:
+	Pool() {
+		Init(0);
+	}
+	Pool(u32 s) {
+		Init(s);
+	}
+	void Free() {
+		free(objs);
+		free(handles);
+		free(erase);
+	}
+	~Pool() {
+		Free();
+	}
+	void Init(u32 s) {
+		size = s;
+		count = 0;
+
+		objs = (T*)calloc(size, sizeof(T));
+		handles = (THandle*)calloc(size, sizeof(THandle));
+		erase = (u32*)calloc(size, sizeof(u32));
+
+		for (u32 i = 0; i < size; i++)
+		{
+			handles[i] = THandle(i, 1);
+			erase[i] = i;
+		}
+	}
+	THandle Add() {
+		if (count >= size) {
+			return THandle::Null();
+		}
+		THandle handle = handles[count++];
+		return handle;
+	}
+	T* Get(const THandle handle) const {
+		if (handle == THandle::Null()) {
+			return nullptr;
+		}
+
+		const u32 arrayIndex = handle.Index();
+		const u32 handleIndex = erase[arrayIndex];
+		const THandle h = handles[handleIndex];
+		if (h != handle) {
+			return nullptr;
+		}
+		return &objs[arrayIndex];
+	}
+	T* operator[](THandle handle) {
+		return Get(handle);
+	}
+	bool Remove(const THandle handle) {
+		const u32 arrayIndex = handle.Index();
+		const u32 handleIndex = erase[arrayIndex];
+		THandle h = handles[handleIndex];
+		if (h != handle) {
+			return false;
+		}
+		handles[handleIndex] = handles[--count];
+		handles[count] = THandle(arrayIndex, h.Generation() + 1);
+
+		const u32 swapIndex = handles[handleIndex].Index();
+		const u32 swap = erase[arrayIndex];
+		erase[arrayIndex] = erase[swapIndex];
+		erase[swapIndex] = swap;
+		return true;
+	}
+	u32 Count() const {
+		return count;
+	}
+	THandle GetHandle(u32 index) const {
+		if (index >= count) {
+			return THandle::Null();
+		}
+		return handles[index];
+	}
+};
