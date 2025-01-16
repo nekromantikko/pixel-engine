6bf8dddb18f8c6306ee973d94b8808b3afb95ed1
Joona Pääkkönen <jonah.paakkonen@gmail.com>
2025-01-16 12:17:16 +0200

    Generate color palette procedurally

--- src/rendering_util.h
+++ src/rendering_util.h
@@ -8,6 +8,8 @@ namespace Rendering
 	{
 		void CreateChrSheet(const char* fname, ChrSheet* outSheet);
 		void LoadPaletteColorsFromFile(const char* fname, u8* outColors);
+		void GeneratePaletteColors(u32* data);
+		void SavePaletteToFile(const char* fname);
 		u8 GetPaletteIndexFromNametableTileAttrib(const Nametable& nametable, s32 xTile, s32 yTile);
 		void CopyMetasprite(const Sprite* src, Sprite* dst, u32 count, IVec2 pos, bool hFlip, bool vFlip);
 		void FlipSpritesHorizontal(Sprite* spr, u32 count);
