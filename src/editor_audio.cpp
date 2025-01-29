405193405e7f2477db7d21833ef996d9b9ef2250
Joona Pääkkönen <jonah.paakkonen@gmail.com>
2025-01-29 03:04:23 +0200

    Rework audio debug UI
    Instead of having to press play, controls modify and display the register state in real time

--- src/editor_audio.cpp
+++ src/editor_audio.cpp
@@ -5,30 +5,6 @@ namespace Editor {
 	namespace Audio {
 		u8 buffer[1024];
 
-		struct PulseControlValues {
-			int duty = 0;
-			int volume = 3;
-			int period = 1024;
-
-			bool sweepEnabled = false;
-			int sweepPeriod = 3;
-			int sweepAmount = 3;
-			bool sweepNegate = false;
-
-			bool useEnvelope = false;
-			int length = -1;
-		};
-
-		PulseControlValues pulses[2];
-
-		struct TriangleControlValues {
-			int period = 1024;
-			int linearPeriod = 64;
-			int length = -1;
-		};
-
-		TriangleControlValues triangle;
-
 		static r32 GetSample(void* data, s32 idx) {
 			return ((r32)((u8*)data)[idx]);
 		}
@@ -38,45 +14,66 @@ namespace Editor {
 
 			ImGui::PushID(idx);
 
-			ImGui::SliderInt("Duty cycle", &pulses[idx].duty, 0, 3);
+			u8 bytes[4];
+			DebugReadPulse(pAudioContext, idx, bytes);
 
-			ImGui::SliderInt("Volume", &pulses[idx].volume, 0, 15);
+			int duty = bytes[0] >> 6;
+			int volume = bytes[0] & 0b00001111;
+			int period = bytes[2] | ((bytes[3] & 0x07) << 8);
+			bool sweepEnabled = (bytes[1] >> 7);
+			int sweepPeriod = (bytes[1] >> 4) & 0x07;
+			int sweepAmount = (bytes[1] & 0x07);
+			bool sweepNegate = (bytes[1] >> 3) & 1;
+			bool useEnvelope = !((bytes[0] >> 4) & 1);
+			bool loop = (bytes[0] >> 5) & 1;
+			int lengthCounterLoad = (bytes[3] >> 3);
 
-			ImGui::SliderInt("Period", &pulses[idx].period, 0, 0x7ff);
-
-			ImGui::Checkbox("Enable sweep", &pulses[idx].sweepEnabled);
-			ImGui::SliderInt("Sweep period", &pulses[idx].sweepPeriod, 0, 7);
-			ImGui::SliderInt("Sweep amount", &pulses[idx].sweepAmount, 0, 7);
-			ImGui::Checkbox("Negate sweep", &pulses[idx].sweepNegate);
-
-			ImGui::Checkbox("Use envelope", &pulses[idx].useEnvelope);
-
-			ImGui::SliderInt("Length (index)", &pulses[idx].length, -1, 31);
-			
-			if (ImGui::Button("Play")) {
-				/*PlayPulse(pAudioContext,
-					idx, 
-					pulses[idx].duty, 
-					//pulses[idx].volume, 
-					pulses[idx].period, 
-					pulses[idx].sweepEnabled,
-					pulses[idx].sweepAmount,
-					pulses[idx].sweepPeriod,
-					pulses[idx].sweepNegate,
-					//!pulses[idx].useEnvelope,
-					pulses[idx].length);*/
-
-				bool loop = pulses[idx].length == -1;
-				u8 lengthCounterLoad = loop ? 0 : pulses[idx].length;
-
-				u8 byte0 = (pulses[idx].duty << 6) | (loop << 5) | (!pulses[idx].useEnvelope << 4) | pulses[idx].volume;
-				u8 byte1 = (pulses[idx].sweepEnabled << 7) | (pulses[idx].sweepPeriod << 4) | (pulses[idx].sweepNegate << 3) | pulses[idx].sweepAmount;
-				u8 byte2 = pulses[idx].period & 0xff;
-				u8 byte3 = (lengthCounterLoad << 3) | (pulses[idx].period >> 8);
-
+			if (ImGui::SliderInt("Duty cycle", &duty, 0, 3)) {
+				u8 byte0 = (duty << 6) | (loop << 5) | (!useEnvelope << 4) | volume;
 				WritePulse(pAudioContext, idx, 0, byte0);
-				WritePulse(pAudioContext, idx, 1, byte1);
+			}
+
+			if (ImGui::SliderInt("Volume", &volume, 0, 15)) {
+				u8 byte0 = (duty << 6) | (loop << 5) | (!useEnvelope << 4) | volume;
+				WritePulse(pAudioContext, idx, 0, byte0);
+			}
+
+			if (ImGui::SliderInt("Period", &period, 0, 0x7ff)) {
+				u8 byte2 = period & 0xff;
+				u8 byte3 = (lengthCounterLoad << 3) | (period >> 8);
 				WritePulse(pAudioContext, idx, 2, byte2);
+				WritePulse(pAudioContext, idx, 3, byte3);
+			}
+
+			if (ImGui::Checkbox("Enable sweep", &sweepEnabled)) {
+				u8 byte1 = (sweepEnabled << 7) | (sweepPeriod << 4) | (sweepNegate << 3) | sweepAmount;
+				WritePulse(pAudioContext, idx, 1, byte1);
+			}
+			if (ImGui::SliderInt("Sweep period", &sweepPeriod, 0, 7)) {
+				u8 byte1 = (sweepEnabled << 7) | (sweepPeriod << 4) | (sweepNegate << 3) | sweepAmount;
+				WritePulse(pAudioContext, idx, 1, byte1);
+			}
+			if (ImGui::SliderInt("Sweep amount", &sweepAmount, 0, 7)) {
+				u8 byte1 = (sweepEnabled << 7) | (sweepPeriod << 4) | (sweepNegate << 3) | sweepAmount;
+				WritePulse(pAudioContext, idx, 1, byte1);
+			}
+			if (ImGui::Checkbox("Negate sweep", &sweepNegate)) {
+				u8 byte1 = (sweepEnabled << 7) | (sweepPeriod << 4) | (sweepNegate << 3) | sweepAmount;
+				WritePulse(pAudioContext, idx, 1, byte1);
+			}
+
+			if (ImGui::Checkbox("Use envelope", &useEnvelope)) {
+				u8 byte0 = (duty << 6) | (loop << 5) | (!useEnvelope << 4) | volume;
+				WritePulse(pAudioContext, idx, 0, byte0);
+			}
+
+			if (ImGui::Checkbox("Loop", &loop)) {
+				u8 byte0 = (duty << 6) | (loop << 5) | (!useEnvelope << 4) | volume;
+				WritePulse(pAudioContext, idx, 0, byte0);
+			}
+
+			if (ImGui::SliderInt("Length counter load", &lengthCounterLoad, 0, 31)) {
+				u8 byte3 = (lengthCounterLoad << 3) | (period >> 8);
 				WritePulse(pAudioContext, idx, 3, byte3);
 			}
 
@@ -93,26 +90,33 @@ namespace Editor {
 			DrawPulseControls(pEditorContext, pAudioContext, 1);
 
 			ImGui::Text("Triangle");
-			ImGui::SliderInt("Period", &triangle.period, 0, 0x7ff);
-			ImGui::SliderInt("Linear period", &triangle.linearPeriod, 0, 0x7f);
-			ImGui::SliderInt("Length (index)", &triangle.length, -1, 31);
 
-			if (ImGui::Button("Play Triangle")) {
-				/*PlayTriangle(pAudioContext,
-					triangle.period,
-					triangle.linearPeriod);*/
+			u8 bytes[4];
+			DebugReadTriangle(pAudioContext, bytes);
 
-				bool loop = triangle.length == -1;
-				u8 lengthCounterLoad = loop ? 0 : triangle.length;
+			int period = bytes[2] | ((bytes[3] & 0x07) << 8);
+			int linearPeriod = bytes[0] & 0x7f;
+			bool loop = bytes[0] >> 7;
+			int lengthCounterLoad = (bytes[3] >> 3);
 
-				u8 byte0 = (loop << 7) | triangle.linearPeriod;
-				u8 byte1 = 0;
-				u8 byte2 = triangle.period & 0xff;
-				u8 byte3 = (lengthCounterLoad << 3) | (triangle.period >> 8);
-
-				WriteTriangle(pAudioContext, 0, byte0);
-				WriteTriangle(pAudioContext, 1, byte1);
+			if (ImGui::SliderInt("Period", &period, 0, 0x7ff)) {
+				u8 byte2 = period & 0xff;
+				u8 byte3 = (lengthCounterLoad << 3) | (period >> 8);
 				WriteTriangle(pAudioContext, 2, byte2);
+				WriteTriangle(pAudioContext, 3, byte3);
+			}
+			if (ImGui::SliderInt("Linear period", &linearPeriod, 0, 0x7f)) {
+				u8 byte0 = (loop << 7) | linearPeriod;
+				WriteTriangle(pAudioContext, 0, byte0);
+			}
+
+			if (ImGui::Checkbox("Loop", &loop)) {
+				u8 byte0 = (loop << 7) | linearPeriod;
+				WriteTriangle(pAudioContext, 0, byte0);
+			}
+
+			if (ImGui::SliderInt("Length counter load", &lengthCounterLoad, 0, 31)) {
+				u8 byte3 = (lengthCounterLoad << 3) | (period >> 8);
 				WriteTriangle(pAudioContext, 3, byte3);
 			}
 
