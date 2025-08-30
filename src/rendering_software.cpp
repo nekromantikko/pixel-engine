53094abd977f0ba47b4b56fd55875d7c0c66c4be
Joona Pääkkönen <jonah.paakkonen@gmail.com>
2025-08-30 10:33:56 +0300

    Render background in software renderer

--- src/rendering_software.cpp
+++ src/rendering_software.cpp
@@ -1,4 +1,5 @@
 #include "rendering.h"
+#include "rendering_util.h"
 #include "core_types.h"
 #include "debug.h"
 #include "memory_arena.h"
@@ -8,14 +9,13 @@ static constexpr u32 FRAMEBUFFER_HEIGHT = VIEWPORT_HEIGHT_PIXELS;
 static constexpr u32 FRAMEBUFFER_SIZE_PIXELS = FRAMEBUFFER_WIDTH * FRAMEBUFFER_HEIGHT;
 static constexpr u32 FRAMEBUFFER_COUNT = 2;
 
-struct Pixel {
-    u8 r;
-    u8 g;
-    u8 b;
+struct Framebuffer {
+    u32 pixels[FRAMEBUFFER_SIZE_PIXELS];
 };
 
-struct Framebuffer {
-    Pixel pixels[FRAMEBUFFER_SIZE_PIXELS];
+struct ColorSample {
+    u8 paletteIndex;
+    u8 colorIndex;
 };
 
 SDL_Window* g_SDLWindow = nullptr;
@@ -30,6 +30,72 @@ ChrSheet* g_ChrSheets;
 Nametable* g_Nametables;
 Scanline* g_Scanlines;
 
+u32* g_paletteColors;
+
+static void ClearFramebuffer(Framebuffer& framebuffer) {
+    memset(framebuffer.pixels, 0, sizeof(framebuffer.pixels));
+}
+
+static glm::uvec2 ScrollScreen(const Scanline& scanline, const glm::uvec2& pixelPos, u32& outNametableIndex) {
+    const glm::uvec2 scanlineScroll = glm::uvec2(scanline.scrollX, scanline.scrollY);
+    glm::uvec2 scrolledPos = pixelPos + scanlineScroll;
+    glm::uvec2 nametablePos = scrolledPos / NAMETABLE_DIM_PIXELS;
+    outNametableIndex = (nametablePos.x + nametablePos.y) % 2;
+    return scrolledPos % NAMETABLE_DIM_PIXELS;
+}
+
+static u8 SampleBackgroundTile(const BgTile& tile, u32 pixelOffset) {
+    if (tile.flipHorizontal) pixelOffset ^= 7;
+    if (tile.flipVertical) pixelOffset ^= 56;
+
+    const u32 chrPage = (tile.tileId >> 8) & 3;
+    const u32 chrTileIndex = tile.tileId & 0xFF;
+    const ChrTile& chrTile = g_ChrSheets[chrPage].tiles[chrTileIndex];
+
+    u8 colorIndex = ((chrTile.p0 >> pixelOffset) & 1);
+    colorIndex |= ((chrTile.p1 >> pixelOffset) & 1) << 1;
+    colorIndex |= ((chrTile.p2 >> pixelOffset) & 1) << 2;
+    return colorIndex;
+}
+
+static ColorSample SampleBackground(const glm::uvec2& scrolledPos, u32 nametableIndex) {
+    glm::uvec2 tilePos = scrolledPos / TILE_DIM_PIXELS;
+    u32 tileIndex = tilePos.x + tilePos.y * NAMETABLE_DIM_TILES;
+    glm::uvec2 pixelPos = scrolledPos % TILE_DIM_PIXELS;
+    u32 pixelIndex = pixelPos.x + pixelPos.y * TILE_DIM_PIXELS;
+    
+    const BgTile& bgTile = g_Nametables[nametableIndex].tiles[tileIndex];
+    u8 colorIndex = SampleBackgroundTile(bgTile, pixelIndex);
+
+    if (colorIndex == 0) {
+        return { 0, 0 }; // Transparent
+    }
+
+    return { u8(bgTile.palette), colorIndex };
+}
+
+static void Draw(Framebuffer& framebuffer) {
+    u32* pPixels = framebuffer.pixels;
+    for (u32 y = 0; y < FRAMEBUFFER_HEIGHT; y++) {
+        Scanline& scanline = g_Scanlines[y];
+        for (u32 x = 0; x < FRAMEBUFFER_WIDTH; x++) {
+            // Apply scroll
+            u32 nametableIndex;
+            glm::uvec2 scrolledPos = ScrollScreen(scanline, glm::uvec2(x, y), nametableIndex);
+
+            // Sample background
+            ColorSample colorSample = SampleBackground(scrolledPos, nametableIndex);
+
+            // Fetch color from palette
+            u8 colorIndex = g_Palettes[colorSample.paletteIndex].colors[colorSample.colorIndex];
+            u32 color = g_paletteColors[colorIndex];
+
+            // Set pixel in framebuffer
+            pPixels[y * FRAMEBUFFER_WIDTH + x] = color;
+        }
+    }
+}
+
 void Rendering::Init(SDL_Window* sdlWindow) {
     g_SDLWindow = sdlWindow;
     g_WindowSurface = SDL_GetWindowSurface(sdlWindow);
@@ -40,8 +106,8 @@ void Rendering::Init(SDL_Window* sdlWindow) {
     g_Framebuffers = ArenaAllocator::PushArray<Framebuffer>(ARENA_PERMANENT, FRAMEBUFFER_COUNT);
 
     for (u32 i = 0; i < FRAMEBUFFER_COUNT; i++) {
-        g_FramebufferSurfaces[i] = SDL_CreateRGBSurfaceFrom(g_Framebuffers[i].pixels, FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, 24, FRAMEBUFFER_WIDTH * sizeof(Pixel),
-            0x0000FF, 0x00FF00, 0xFF0000, 0);
+        g_FramebufferSurfaces[i] = SDL_CreateRGBSurfaceFrom(g_Framebuffers[i].pixels, FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT, 32, FRAMEBUFFER_WIDTH * sizeof(u32),
+            0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
     }
 
     g_Palettes = ArenaAllocator::PushArray<Palette>(ARENA_PERMANENT, PALETTE_COUNT);
@@ -50,11 +116,8 @@ void Rendering::Init(SDL_Window* sdlWindow) {
     g_Nametables = ArenaAllocator::PushArray<Nametable>(ARENA_PERMANENT, NAMETABLE_COUNT);
     g_Scanlines = ArenaAllocator::PushArray<Scanline>(ARENA_PERMANENT, SCANLINE_COUNT);
 
-    for (u32 i = 0; i < FRAMEBUFFER_COUNT; i++) {
-        for (u32 p = 0; p < FRAMEBUFFER_SIZE_PIXELS; p++) {
-            g_Framebuffers[i].pixels[p] = { 100, 149, 237 }; // Cornflower Blue
-        }
-    }
+    g_paletteColors = ArenaAllocator::PushArray<u32>(ARENA_PERMANENT, COLOR_COUNT);
+    Rendering::Util::GeneratePaletteColors(g_paletteColors);
 }
 
 void Rendering::Free() {
@@ -72,6 +135,8 @@ void Rendering::BeginRenderPass() {
 }
 
 void Rendering::EndFrame() {
+    ClearFramebuffer(g_Framebuffers[0]);
+    Draw(g_Framebuffers[0]);
     SDL_BlitScaled(g_FramebufferSurfaces[0], nullptr, g_WindowSurface, nullptr);
     SDL_UpdateWindowSurface(g_SDLWindow);
 }
